a1037c8d877290323f69705621258ef794949f7c
RUBEN <ruben.gamarra@exxis-group.com>
2024-03-14 09:18:28 -0500

    update transact

--- 1. Interface/SAP/Resources/Query/proc_transac.sql
+++ 1. Interface/SAP/Resources/Query/proc_transac.sql
@@ -1,9 +1,11 @@
 
+
 if (:object_type = '15'  AND (:transaction_type = 'A' OR :transaction_type = 'U') and IFNULL(:error,0)=0) 
 THEN
 
 		select "U_EXX_HOAS_STAD" INTO estadoSUNAT 	FROM ODLN T0 
-										where T0."DocEntry"=:list_of_cols_val_tab_del ;
+										where COALESCE(T0."U_EXX_ADDON",'') NOT IN ('FMS','ECM','WTP')
+										AND  T0."DocEntry"=:list_of_cols_val_tab_del ;
 		 IF(:estadoSUNAT='A')THEN
 		 	error := 1000;
 			error_message := N'No puede crear la guía con el campo Estado Envío Sunat: Sin Asignar';
@@ -11,6 +13,7 @@ THEN
 
 
 END IF;
+
 
 ALTER PROCEDURE "SP_EXXIS_FE_FOLIOS"
 (
